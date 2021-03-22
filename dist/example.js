192c951e921fe87b9d0ae60bc258943e8cc47049
nightDrizle <1563607949@qq.com>
2021-03-22 20:34:39 +0800

    'generate babel source map'

---
+++ dist/example.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var getData = function getData() {
+  var data = [1, 2, 3];
+  return data;
+};
+//# sourceMappingURL=example.js.map
---
+++ src/example.js
@@ -0,0 +1,4 @@
+const getData = () => {
+  const data = [1, 2, 3];
+  return data;
+};
